eba9880c993e1d110eb47243f9998a3d5ea63e72
Dilan1502 <dbb.haro@yavirac.edu.ec>
2023-11-26 20:55:44 -0500

    front ya valeee

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AnimalComponent } from './animal/animal.component';
+
+const routes: Routes = [{path:"animal",component:AnimalComponent}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
